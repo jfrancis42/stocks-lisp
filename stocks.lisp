e5d85e5652506ec9cae601dbb8cb5d444d4e4d99
Jeff Francis <jeff@gritch.org>
2017-09-01 13:26:50 -0700

    Oops, should return an alist, not a list of alists.

--- stocks.lisp
+++ stocks.lisp
@@ -3,11 +3,12 @@
 (defun get-stock (stock)
   "Given a stock (examples: PANW, CHKP), return the available data for
 that stock from the Google Finance API."
-  (json:decode-json-from-string
-   (subseq
-    (first
-     (multiple-value-list
-      (drakma:http-request
-       (concatenate 'string "https://finance.google.com/finance/info?client=ig&q=" stock)
-       :method :get)))
-    4)))
+  (first
+   (json:decode-json-from-string
+    (subseq
+     (first
+      (multiple-value-list
+       (drakma:http-request
+	(concatenate 'string "https://finance.google.com/finance/info?client=ig&q=" stock)
+	:method :get)))
+     4))))
